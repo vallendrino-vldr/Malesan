da5611a3efe3e2959f94420d682588684d12bdd5
vadlyvldr <vadlyvldr@gmail.com>
2026-08-30 20:56:39 +0700

    perf(clip): format string optimized for fast HD extraction

--- android/app/src/main/java/id/my/malesan/app/NativeClipEngine.java
+++ android/app/src/main/java/id/my/malesan/app/NativeClipEngine.java
@@ -35,7 +35,7 @@ final class NativeClipEngine {
                         .addOption("--no-playlist")
                         .addOption("--no-part")
                         .addOption("--no-warnings")
-                        .addOption("--format", "22/bv*[height<=1080][ext=mp4]+ba[ext=m4a]/bv*[height<=1080]+ba/b[height<=1080][ext=mp4]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/best")
+                        .addOption("--format", "best[height<=1080][ext=mp4]/best[height<=720][ext=mp4]/bv*[height<=1080][ext=mp4]+ba[ext=m4a]/best")
                         .addOption("--merge-output-format", "mp4")
                         .addOption("--download-sections", String.format(Locale.US, "*%.3f-%.3f", startSeconds, endSeconds))
                         .addOption("--force-keyframes-at-cuts")
